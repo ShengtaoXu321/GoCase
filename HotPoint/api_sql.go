e83cd9a0e5c9258eda50437b0feef88c6353ba63
徐胜涛 <3070448639@qq.com>
2021-04-08 10:39:39 +0800

    新增了api访问数据库

--- HotPoint/api_sql.go
+++ HotPoint/api_sql.go
@@ -3,8 +3,6 @@ package main
 import (
 	"database/sql"
 	"encoding/json"
-	"fmt"
-	"github.com/gin-gonic/gin"
 	_ "github.com/go-sql-driver/mysql"
 	"io/ioutil"
 	"log"
@@ -16,7 +14,7 @@ import (
 // 2. 利用Gin返回数据库中内容，能够获取到热点数据
 
 // 1.1 反序列化
-type Hots []struct {
+type Hots1 []struct {
 	Node struct {
 		AvatarLarge      string        `json:"avatar_large"`
 		Name             string        `json:"name"`
@@ -63,15 +61,13 @@ type Hots []struct {
 	ID              int    `json:"id"`
 }
 
-//定义数据库结构体
-type HotPoint struct {
+type Result1 struct {
 	Title string
 	Url   string
 }
 
-// 主函数
 func main() {
-	var db *sql.DB
+	var db1 *sql.DB
 	// 第一步：将api的数据获取下来
 	r, err := http.Get("https://www.v2ex.com/api/topics/hot.json")
 	if err != nil {
@@ -79,7 +75,7 @@ func main() {
 	}
 	defer r.Body.Close()
 	rBody, _ := ioutil.ReadAll(r.Body)
-	var res Hots
+	var res Hots1
 	err = json.Unmarshal(rBody, &res) // 将json字符串数据解码到相应的数据结构；Unmaeshal的第一个参数是json字符串，第二个参数是接受json解析的数据结构
 	if err != nil {
 		log.Println("json字符串解析失败")
@@ -93,24 +89,23 @@ func main() {
 	//fmt.Println(newResTit)
 	//fmt.Println(newResUrl)
 
-	// 1. 初始化数据库
-	db, err1 := SqlInit()
+	// 初始化数据库
+	db1, err1 := SqlInit1()
 	if err1 != nil {
 		log.Println("数据库初始化失败", err)
 	}
 
-	defer db.Close()
-
-	// 2. 元素赋值+SQL插入
+	defer db1.Close()
+	// 元素赋值+SQL插入
 	length := len(newResTit)
-	A := make([]HotPoint, length)
+	A := make([]Result1, length)
 	for i := 0; i < length; i++ {
 		A[i].Title = newResTit[i]
 		A[i].Url = newResUrl[i]
 		s1 := A[i].Title
 		s2 := A[i].Url
 		sen := "INSERT INTO hotpoint(title,url) VALUES (?,?)"
-		_, err1 := db.Exec(sen, s1, s2)
+		_, err1 := db1.Exec(sen, s1, s2)
 
 		if err1 != nil {
 			log.Println("数据插入失败", err1)
@@ -118,52 +113,21 @@ func main() {
 
 	}
 
-	// 3. 创建api
-	router := gin.Default() // 使用Default()方法来获取一个基本的路由变量
-	// API处理程序 -- 获取用户详细信息
-	router.GET("/hotpoint/:title", func(c *gin.Context) { // 使用匿名函数作为路由的处理函数，处理函数必须是func(*gin.Context)类型的函数
-		var (
-			hp     HotPoint
-			result gin.H // gin.H() 方法简化json的生成，本质就是一个map[string]interface{}
-		)
-		title := c.Param("title")
-		fmt.Println("输入年龄:", title)
-		sen1 := "SELECT title,url from hotpoint where title=?"
-		row := db.QueryRow(sen1, title)
-		err = row.Scan(&hp.Title, &hp.Url)
-		fmt.Printf("用户: %+v\n", hp)
-		if err != nil {
-			result = gin.H{
-				"hp":    nil,
-				"count": 0,
-			}
-		} else {
-			result = gin.H{
-				"title": hp.Title,
-				"url":   hp.Url,
-				"count": 1,
-			}
-		}
-		c.JSON(http.StatusOK, result)
-
-	})
-	router.Run(":18510")
-
 }
 
 // 数据库初始化函数
-func SqlInit() (db *sql.DB, err error) {
+func SqlInit1() (db1 *sql.DB, err error) {
 	config := "root:123456@tcp(117.78.34.82:18100)/Hots?charset=utf8mb4"
-	db, err = sql.Open("mysql", config)
+	db1, err = sql.Open("mysql", config)
 	if err != nil {
 		log.Println("数据库连接失败", err)
 	}
-	db.SetMaxIdleConns(100)
-	db.SetMaxOpenConns(100)
+	db1.SetMaxIdleConns(100)
+	db1.SetMaxOpenConns(100)
 	// 判断连通性
-	err = db.Ping()
+	err = db1.Ping()
 	if err != nil {
 		log.Println("数据库不能连通", err)
 	}
-	return db, err
+	return db1, err
 }
--- HotPoint/creat_api_sql.go
+++ HotPoint/creat_api_sql.go
@@ -1,5 +1,3 @@
-// 这是创建api进行访问数据库的demo -- 参考网上
-
 package main
 
 import (
@@ -11,7 +9,13 @@ import (
 	"net/http"
 )
 
+type HotPoint1 struct {
+	Title string
+	Url   string
+}
+
 func main() {
+	// 1. 数据库连接
 	config := "root:123456@tcp(117.78.34.82:18100)/Hots?charset=utf8mb4"
 	db, err := sql.Open("mysql", config)
 	if err != nil {
@@ -24,41 +28,35 @@ func main() {
 		log.Println("数据库不能正常连接", err)
 	}
 
-	// 定义数据库的内容结构体
-	type User struct {
-		age  int
-		name string
-	}
-
-	// 定义一个路由
+	// 2. 创建api
 	router := gin.Default() // 使用Default()方法来获取一个基本的路由变量
 	// API处理程序 -- 获取用户详细信息
-	router.GET("/person/:age", func(c *gin.Context) { // 使用匿名函数作为路由的处理函数，处理函数必须是func(*gin.Context)类型的函数
+	router.GET("/hotpoint/:title", func(c *gin.Context) { // 使用匿名函数作为路由的处理函数，处理函数必须是func(*gin.Context)类型的函数
 		var (
-			user   User
+			hp1    HotPoint1
 			result gin.H // gin.H() 方法简化json的生成，本质就是一个map[string]interface{}
 		)
-		age := c.Param("age")
-		fmt.Println("输入年龄:", age)
-		s1 := "SELECT age,name from user where age=?"
-		row := db.QueryRow(s1, age)
-		err = row.Scan(&user.age, &user.name)
-		fmt.Printf("用户: %+v\n", user)
+		title := c.Param("title")
+		fmt.Println("输入标题:", title)
+		sen1 := "SELECT title, url FROM hotpoint WHERE title=?"
+		row := db.QueryRow(sen1, title)
+		err = row.Scan(&hp1.Title, &hp1.Url)
+		fmt.Printf("用户: %+v\n", hp1)
 		if err != nil {
 			result = gin.H{
-				"user":  nil,
+				"hp":    nil,
 				"count": 0,
 			}
+			log.Println(err)
 		} else {
 			result = gin.H{
-				"age":   user.age,
-				"name":  user.name,
+				"title": hp1.Title,
+				"url":   hp1.Url,
 				"count": 1,
 			}
 		}
 		c.JSON(http.StatusOK, result)
 
 	})
-	router.Run(":18600")
-
+	router.Run(":18520")
 }
