fc85890166a7ae72026a72c6aa931e72324cb94a
徐胜涛 <3070448639@qq.com>
2021-04-08 11:25:29 +0800

    新增了api访问数据库，修复bug

--- HotPoint/creat_api_sql.go
+++ HotPoint/creat_api_sql.go
@@ -10,6 +10,7 @@ import (
 )
 
 type HotPoint1 struct {
+	Id    int
 	Title string
 	Url   string
 }
@@ -31,17 +32,17 @@ func main() {
 	// 2. 创建api
 	router := gin.Default() // 使用Default()方法来获取一个基本的路由变量
 	// API处理程序 -- 获取用户详细信息
-	router.GET("/hotpoint/:title", func(c *gin.Context) { // 使用匿名函数作为路由的处理函数，处理函数必须是func(*gin.Context)类型的函数
+	router.GET("/hotpoint/:id", func(c *gin.Context) { // 使用匿名函数作为路由的处理函数，处理函数必须是func(*gin.Context)类型的函数
 		var (
 			hp1    HotPoint1
 			result gin.H // gin.H() 方法简化json的生成，本质就是一个map[string]interface{}
 		)
-		title := c.Param("title")
-		fmt.Println("输入标题:", title)
-		sen1 := "SELECT title, url FROM hotpoint WHERE title=?"
-		row := db.QueryRow(sen1, title)
-		err = row.Scan(&hp1.Title, &hp1.Url)
-		fmt.Printf("用户: %+v\n", hp1)
+		id := c.Param("id")
+		fmt.Println("输入ID:", id)
+		sen1 := "SELECT id,title, url FROM hotpoint WHERE id=?"
+		row := db.QueryRow(sen1, id)
+		err = row.Scan(&hp1.Id, &hp1.Title, &hp1.Url)
+		fmt.Printf("热点数据: %+v\n", hp1)
 		if err != nil {
 			result = gin.H{
 				"hp":    nil,
@@ -50,9 +51,9 @@ func main() {
 			log.Println(err)
 		} else {
 			result = gin.H{
+				"id":    hp1.Id,
 				"title": hp1.Title,
 				"url":   hp1.Url,
-				"count": 1,
 			}
 		}
 		c.JSON(http.StatusOK, result)
